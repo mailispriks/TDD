6c063ff1b4b32f4c94959b0c25220f43b8b1d0d1
Mailis Kokla <mailis@fatarrow.net>
2021-03-30 12:22:16 +0300

    Add greet

--- greet.js
+++ greet.js
@@ -1,1 +1,15 @@
-module.exports = null
+function isUpperCase(str) {
+  return str === str.toUpperCase();
+}
+
+const greet = (name) => {
+  let correctName = null
+  if (Array.isArray(name)) {
+    correctName = isUpperCase(name.join()) ? name.join(' AND ') : name.join(' and ')
+  } else {
+    correctName = name || 'my friend'
+  }
+  return isUpperCase(correctName) ? `HELLO ${correctName}!` : `Hello, ${correctName}.`
+} 
+
+module.exports = greet
--- greet.test.js
+++ greet.test.js
@@ -1,5 +1,24 @@
 const greet = require('./greet')
 
 describe('greet', () => {
-  
+  it('should greet Bob', () => {
+    expect(greet('Bob')).toBe('Hello, Bob.')
+  })
+
+  it('should greet friend, when no name is defined', () => {
+    expect(greet()).toBe('Hello, my friend.')
+  })
+
+  it('should handle shouting', () => {
+    expect(greet('JERRY')).toBe('HELLO JERRY!')
+  })
+
+  it('should handle two names', () => {
+    expect(greet(['Jill', 'Jane'])).toBe('Hello, Jill and Jane.')
+  })
+
+  it('should handle two names shouting', () => {
+    expect(greet(['DANA', 'TOM'])).toBe('HELLO DANA AND TOM!')
+  })
+
 })
